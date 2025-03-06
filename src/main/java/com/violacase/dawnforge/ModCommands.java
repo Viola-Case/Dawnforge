395333b2cc72c17647e2cb44678f8c17ee7b7670
viola-case <ajr.acnt@gmail.com>
2025-03-06 14:05:00 -0600

    initial commit

---
+++ src/main/java/com/violacase/dawnforge/ModCommands.java
@@ -0,0 +1,4 @@
+package com.violacase.dawnforge;
+
+public class ModCommands {
+}
---
+++ src/main/java/com/violacase/dawnforge/ModItems.java
@@ -0,0 +1,31 @@
+package com.violacase.dawnforge;
+
+import com.violacase.dawnforge.items.*;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static com.violacase.dawnforge.Dawnforge.MODID;
+
+public class ModItems {
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    // Create a Deferred Register to hold Items which will all be registered under the "dawnforge" namespace
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "dawnforge" namespace
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public static final RegistryObject<Item> SUN_GEM = ITEMS.register("sun_gem", () -> new SunGemItem(new Item.Properties().rarity(Rarity.EPIC)));
+
+    public static final void Register(IEventBus modEventBus) {
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+        CREATIVE_MODE_TABS.register(modEventBus);
+    }
+}
